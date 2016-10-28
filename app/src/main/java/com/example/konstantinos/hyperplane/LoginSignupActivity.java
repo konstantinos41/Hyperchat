a125aa32839c41aadd997aa69581276852339ef7
Konstantinos Mavrodis <konstantinos41@hotmail.com>
2016-10-28 13:57:04 +0300

    Adds some files

---
+++ app/src/main/java/com/example/konstantinos/hyperplane/LoginSignupActivity.java
@@ -0,0 +1,142 @@
+package com.example.konstantinos.hyperplane;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
+import com.applozic.mobicomkit.api.account.user.User;
+import com.applozic.mobicomkit.api.account.user.UserLoginTask;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.internal.CallbackManagerImpl;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+
+public class LoginSignupActivity extends AppCompatActivity
+{
+    private CallbackManager callbackManager;
+    private TextView info;
+    private LoginButton loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        //AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login_signup);
+        info = (TextView)findViewById(R.id.info);
+
+
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions( Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User ID:  " +
+                        loginResult.getAccessToken().getUserId() + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken());
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    final JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                final JSONObject jsonObject = response.getJSONObject();
+                                String nombre = "";
+                                String email = "";
+                                String id = "";
+                                try {
+                                    nombre = jsonObject.getString("name");
+                                    email =  jsonObject.getString("email");
+                                    id = jsonObject.getString("id");
+
+                                    System.out.println(nombre);
+                                    System.out.println(email);
+                                    System.out.println(id);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                loginSignupToApplozic(nombre,email,id);
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText(e.toString());
+            }
+
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    public void loginSignupToApplozic(String name, String email, String id)
+    {
+        UserLoginTask.TaskListener listener = new UserLoginTask.TaskListener() {
+
+            @Override
+            public void onSuccess(RegistrationResponse registrationResponse, Context context) {
+                //After successful registration with Applozic server the callback will come here
+            }
+
+            @Override
+            public void onFailure(RegistrationResponse registrationResponse, Exception exception) {
+                //If any failure in registration the callback  will come here
+            }};
+
+
+        User user = new User();
+        user.setUserId(id); //userId it can be any unique user identifier
+        user.setDisplayName(name); //displayName is the name of the user which will be shown in chat messages
+        user.setEmail(email); //optional
+        //user.setImageLink("");//optional,pass your image link
+        new UserLoginTask(user, listener, LoginSignupActivity.this).execute((Void) null);
+    }
+}
---
+++ app/src/main/java/com/example/konstantinos/hyperplane/TodoItem.java
@@ -0,0 +1,10 @@
+package com.example.konstantinos.hyperplane;
+
+/**
+ * Created by Konstantinos on 10/24/2016.
+ */
+
+public class TodoItem {
+    public String Id;
+    public String Text;
+}

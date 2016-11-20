20038e4606bf46c84456dc5a081b7fa1d314b8b0
andreash92 <andreas_h92@hotmail.com>
2016-11-20 13:39:59 +0200

    added invite function. - To do: configure the app URL.

--- app/src/main/java/com/example/konstantinos/hyperplane/FriendsFragment.java
+++ app/src/main/java/com/example/konstantinos/hyperplane/FriendsFragment.java
@@ -12,15 +12,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.share.model.AppInviteContent;
+import com.facebook.share.widget.AppInviteDialog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -67,6 +71,7 @@ public class FriendsFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_friends, container, false);
 
         mainListView = (ListView) rootView.findViewById( R.id.friends_list );
+        mainListView.setClickable(true);
 
         // Check if friends list is empty and if it is fill it with friends
         // There seems to be a problem when a lot of friends are on the list, it takes some time to load
@@ -99,7 +104,40 @@ public class FriendsFragment extends Fragment {
         }
 
 
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Object obj = mainListView.getItemAtPosition(position);
+
+                //check if the invite button is clicked
+                if (obj.equals("Invite more Friends!")){
+                    inviteFriends();
+                }
+
+                /* do something more */
+            }
+        });
 
         return rootView;
     }
+
+    
+    // implements the "invite friends" function
+    public void inviteFriends(){
+        String appLinkUrl, previewImageUrl;
+
+        appLinkUrl = "https://www.google.com";
+        previewImageUrl = "https://lh5.ggpht.com/DZG1niZb55ylVexAwfs19GHsY7kjt_onLmhtuAdQaZmPDpxHkYrpXI8iV1eGAlgm9N0=w300-rw";
+
+        if (AppInviteDialog.canShow()) {
+            AppInviteContent content = new AppInviteContent.Builder()
+                    .setApplinkUrl(appLinkUrl)
+                    .setPreviewImageUrl(previewImageUrl)
+                    .build();
+            AppInviteDialog.show(this, content);
+        }
+    }
+
+
+
 }
